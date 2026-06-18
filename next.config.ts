fbaba52df498187fa81ab07fefd77c3d58db429e
Nikita Semenov <mrkitnik@gmail.com>
2026-06-18 16:18:51 +0200

    configured github actions deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true, // Required for static export as Next.js image optimization is not supported on static hosting
+  },
+  // Remove or comment out the next line if deploying to minorflunky.github.io
+  basePath: '/portfolio', 
 };
 
-export default nextConfig;
+module.exports = nextConfig;
